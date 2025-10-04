ecf4bd8cb88408611a59b75f200e227f2e592c27
Dani Gergely <daniel.gergely.2005@gmail.com>
2025-10-04 13:38:43 +0200

    init fastapi and db

---
+++ backend/src/db/database.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+load_dotenv()
+
+TORTOISE_ORM = {
+    "connections": {"default": os.environ.get("DATABASE_URL")},
+    "apps": {
+        "models": {
+            "models": ["app.models.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["models"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
